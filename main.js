a28a74c149d419e3e7e5390c98fe4f0bd12a3b5f
Fabien Marceau <fabien.marceau@gmail.com>
2023-03-09 23:01:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// const datas = [];
+
+// fetch("https://api.meteo-concept.com/api/ephemeride/0?token=77318a42b3d89e38aaccf4d3829c357c197b8601be0095d98d129c5b9f2ef9b8")
+//   .then((response) => {
+//     datas = response.data;
+//     console.log(datas);
+//   })
+  
+
+
+// fetch("https://api.meteo-concept.com/api/ephemeride/0?token=77318a42b3d89e38aaccf4d3829c357c197b8601be0095d98d129c5b9f2ef9b8")
+//   .then(response => response.text())
+//   .then(body=>console.log(body))
+
+// let donnee = [];
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   // .then((body) => console.log(body));
+
+
+const d = new Date("March 21, 2023 09:41:20");
+let day = d.getDate();
+console.log(day);
+  
+
